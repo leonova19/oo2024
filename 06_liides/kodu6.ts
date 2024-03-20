9a6b344b8946b0c8826990b2a03fc93f60a95cd0
Eliana <146323849+leonova19@users.noreply.github.com>
2024-03-20 18:12:43 +0200

    Update kodu6.ts

--- 06_liides/kodu6.ts
+++ 06_liides/kodu6.ts
@@ -1,1 +1,28 @@
+interface Adder{
+   arvutaPindala(nr: number):number;
+   arvutaÜmbermõõt():number;
+}
+
+class Ristkülik implements Adder{ 
+    private pikkus: number;
+    private laius: number;
+
+    constructor(pikkus: number, laius: number) {
+        this.pikkus = pikkus;
+        this.laius = laius;
+    }
+
+    arvutaPindala(): number {
+        return this.pikkus * this.laius;
+    }
+
+    arvutaÜmbermõõt(): number {
+        return 2 * (this.pikkus + this.laius);
+    }
+}
+
+
+const ristkülik = new Ristkülik(5, 4);
+console.log("Ristküliku pindala:", ristkülik.arvutaPindala());
+console.log("Ristküliku ümbermõõt:", ristkülik.arvutaÜmbermõõt());
 
